c0c7cafc96b3198456b7f1c9d5657cd0bbaafd0b
Henrik Schletter <62748382+Henrik168@users.noreply.github.com>
2021-11-21 22:07:34 +0100

    Add files via upload

--- six_sigma/plot.py
+++ six_sigma/plot.py
@@ -1,62 +1,97 @@
 import matplotlib.pyplot as plt
 from six_sigma.calc import mean, stddev
-
-LIMIT_COLOR = (0.8, 0.6, 0.0, 0.7)
-
-
-def create_plot(data: list, upper_limit: float = 0.0, lower_limit: float = 0.0, dataset_name: str = None):
-    mean_value = mean(data)
-    stddev_value = stddev(data)
-    upper_3s = min((mean_value + 3 * stddev_value), 100)
-    lower_3s = max((mean_value - 3 * stddev_value), 0)
-
-    if dataset_name:
-        plt.title(f"Data of: {dataset_name}")
-    plt.xlabel("Number of Value")
-    plt.ylabel("Quality [%]")
-
-    plt.axis([0, len(data), 0, 100])
-    for index, element in enumerate(data):
-        if upper_limit and element > upper_limit:
-            plt.plot(index,element, "ro")
-        if lower_limit and element < lower_limit:
-            plt.plot(index,element, "ro")
-        else:
-            plt.plot(index,element, "go")
-
-    plt.axhline(y=mean_value, color='b', linestyle='-.')
-    plt.axhline(y=upper_3s, color=LIMIT_COLOR, linestyle='--')
-    plt.axhline(y=lower_3s, color=LIMIT_COLOR, linestyle='--')
-    if upper_limit:
-        plt.axhline(y=upper_limit, color='r', linestyle='-')
-    if lower_limit:
-        plt.axhline(y=lower_limit, color='r', linestyle='-')
-    plt.show()
+from typing import Tuple
 
 
-def create_histogramm(data: list, upper_limit: float = 0.0, lower_limit: float = 0.0, dataset_name: str = None):
-    mean_value = mean(data)
-    stddev_value = stddev(data)
-    upper_3s = min((mean_value + 3 * stddev_value), 100)
-    lower_3s = max((mean_value - 3 * stddev_value), 0)
+class Plot:
+    LIMIT_COLOR = (0.8, 0.6, 0.0, 0.7)
 
-    if dataset_name:
-        plt.title(f"Histogram of: {dataset_name}")
-    plt.xlabel('Quality [%]')
-    plt.ylabel('Count')
+    def __init__(self,
+                 y_label: str = None,
+                 x_data_label: str = None,
+                 x_hist_label: str = None,
+                 y_min: float = None,
+                 y_max: float = None):
+        self.figure_index: int = 0
+        self.figures = dict()
+        self.y_label = y_label
+        self.x_data_label = x_data_label
+        self.x_hist_label = x_hist_label
+        self.y_min = y_min
+        self.y_max = y_max
 
-    count_bins = int((max(data) - min(data)) / 2)
+    @staticmethod
+    def calc_sigma(data: list) -> Tuple:
+        mean_value = mean(data)
+        stddev_value = stddev(data)
+        upper_3s = min((mean_value + 3 * stddev_value), 100)
+        lower_3s = max((mean_value - 3 * stddev_value), 0)
+        return mean_value, stddev_value, lower_3s, upper_3s
 
-    n, bins, patches = plt.hist(data, count_bins, density=False, facecolor='g')
+    @staticmethod
+    def in_bounds(lower, value, upper) -> bool:
+        return lower <= value <= upper
 
-    plt.axis([min(bins) - 2, max(bins) + 2, 0, max(n) + 2])
-    plt.grid(True)
+    @staticmethod
+    def plot_hist(data, ax_hist):
+        # no labels
+        ax_hist.tick_params(axis="y", labelleft=False)
 
-    plt.axvline(x=mean_value, color='b', linestyle='-.')
-    plt.axvline(x=upper_3s, color=LIMIT_COLOR, linestyle='--')
-    plt.axvline(x=lower_3s, color=LIMIT_COLOR, linestyle='--')
-    if upper_limit:
-        plt.axvline(x=upper_limit, color='r', linestyle='-')
-    if lower_limit:
-        plt.axvline(x=lower_limit, color='r', linestyle='-')
-    plt.show()
+        # now determine nice limits by hand:
+        binwidth = 2
+        xmax = max(data)
+        lim = (int(xmax / binwidth) + 1) * binwidth
+
+        bins = [bin1 for bin1 in range(-lim, lim + binwidth, binwidth)]
+        ax_hist.hist(data, bins=bins, orientation='horizontal')
+
+    def plot_scatter(self, data, ax, lower_limit, upper_limit):
+        mean_value, stddev_value, lower_3s, upper_3s = self.calc_sigma(data)
+        y_min = self.y_min if self.y_min is not None else min(data)
+        y_max = self.y_max if self.y_max is not None else max(data)
+        # the scatter plot:
+        ax.axis([0, len(data), y_min, y_max])
+
+        for index, value in enumerate(data):
+            if not self.in_bounds(lower_limit, value, upper_limit):
+                ax.plot(index, value, "ro")
+            else:
+                ax.plot(index, value, "go")
+
+        ax.axhline(y=mean_value, color='b', linestyle='-.')
+        ax.axhline(y=upper_3s, color=self.LIMIT_COLOR, linestyle='--')
+        ax.axhline(y=lower_3s, color=self.LIMIT_COLOR, linestyle='--')
+        ax.axhline(y=upper_limit, color='r', linestyle='-')
+        ax.axhline(y=lower_limit, color='r', linestyle='-')
+
+    def add_plot(self, data: list, title: str, upper_limit: float, lower_limit: float) -> None:
+
+        fig = plt.figure(num=self.figure_index, figsize=[10, 7])
+        gs = fig.add_gridspec(1, 2, width_ratios=(7, 3),
+                              left=0.1, right=0.9, bottom=0.2, top=0.9,
+                              wspace=0.1, hspace=0.05)
+
+        ax = fig.add_subplot(gs[0, 0])
+        ax_hist = fig.add_subplot(gs[0, 1], sharey=ax)
+
+        # use the previously defined function
+        self.plot_hist(data, ax_hist)
+        self.plot_scatter(data, ax, lower_limit, upper_limit)
+
+        plt.suptitle(title)
+        ax.tick_params(axis='x', labelrotation=45)
+        ax.set(xlabel=self.x_data_label, ylabel=self.y_label)
+        ax_hist.set(xlabel=self.x_hist_label)
+
+        ax_hist.tick_params(axis='x', labelrotation=45)
+
+        self.figure_index += 1
+        self.figures[title] = fig
+
+    @staticmethod
+    def show_plots() -> None:
+        plt.show()
+
+    def save_plots(self) -> None:
+        for key, fig in self.figures.items():
+            fig.savefig(f"{key}_fig.png")
