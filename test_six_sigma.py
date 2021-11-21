f808e1118957cc03fe134ab98800baa3632f3017
Henrik Schletter <62748382+Henrik168@users.noreply.github.com>
2021-11-21 22:07:11 +0100

    Add files via upload

--- test_six_sigma.py
+++ test_six_sigma.py
@@ -1,13 +1,19 @@
 from six_sigma.calc import mean
-from random import random, gauss
-from six_sigma.calc import mean,stddev
-from six_sigma.plot import create_plot, create_histogramm
+from random import random, gauss, randrange
+from six_sigma.calc import mean, stddev
+from six_sigma.plot import Plot
 
-test_data = [min(random() * 100,100) for _ in range(1000)]
-test_data_gaus = [min(gauss(mu=90, sigma=10),100) for _ in range(100)]
-test_data_gaus.append(50)
-test_data_gaus.append(55)
-test_data_gaus.append(59)
+test_data = [randrange(50, 80) for _ in range(1000)]
+test_data_gaus = [min(gauss(mu=97, sigma=2), 100) for _ in range(1000)]
+test_data_gaus2 = [min(gauss(mu=90, sigma=10), 100) for _ in range(1000)]
 
-create_plot(test_data_gaus,lower_limit=80,dataset_name="test1")
-create_histogramm(test_data_gaus,lower_limit=80,dataset_name="test1")
+plt = Plot(y_label="Y-Label",
+           x_data_label="X-Data",
+           x_hist_label="X-Hist",
+           y_min=0,
+           y_max=100)
+plt.add_plot(test_data_gaus, "GausData1", 100, 80)
+plt.add_plot(test_data_gaus2, "GausData2", 100, 80)
+plt.add_plot(test_data, "RandData", 75, 55)
+#plt.show_plots()
+plt.save_plots()
