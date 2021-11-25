42ff95104911aa4bd251ac7442fd2d0ab627c6a7
Henrik Schletter <62748382+Henrik168@users.noreply.github.com>
2021-11-25 19:10:21 +0100

    Add files via upload

--- test_six_sigma.py
+++ test_six_sigma.py
@@ -1,9 +1,10 @@
 from random import gauss, randrange, seed
 from six_sigma.plot import Plot
+from lib_path import make_path
 
-seed(42)
+#seed(42)
 test_data = [randrange(50, 80) for _ in range(1000)]
-test_data_gauss = [min(gauss(mu=97, sigma=2), 100) for _ in range(1000)]
+test_data_gauss = [min(gauss(mu=95, sigma=5), 100) for _ in range(1000)]
 test_data_gauss2 = [min(gauss(mu=90, sigma=10), 100) for _ in range(1000)]
 
 plt = Plot(y_label="Y-Label",
@@ -12,9 +13,13 @@ plt = Plot(y_label="Y-Label",
            y_min=0,
            y_max=100,
            bin_width=2)
+#plt = Plot(y_label="Y-Label",
+#           x_data_label="X-Data",
+#           x_hist_label="X-Hist",
+#           bin_width=2)
 
 plt.add_plot(test_data_gauss, "GaussData1", 100, 80)
 plt.add_plot(test_data_gauss2, "GaussData2", 100, 80)
 plt.add_plot(test_data, "RandData", 75, 55)
 plt.show_plots()
-plt.save_plots()
+plt.save_plots(dir_path=make_path("./test/"))
