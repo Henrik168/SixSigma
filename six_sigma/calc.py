3f4172d8e27d3f1355341646b7018f6020257dbf
Henrik Schletter <62748382+Henrik168@users.noreply.github.com>
2021-11-25 19:11:27 +0100

    add median

--- six_sigma/calc.py
+++ six_sigma/calc.py
@@ -3,15 +3,25 @@ from typing import List
 
 
 def mean(value_list: List[float]) -> float:
-    return sum(value_list) / len(value_list)
+    return float(sum(value_list) / len(value_list))
 
 
 def variance(value_list: List[float], ddof: int = 0) -> float:
     mean_list = mean(value_list)
-    return sum((value - mean_list) ** 2 for value in value_list) / (len(value_list) - ddof)
+    return float(sum((value - mean_list) ** 2 for value in value_list) / (len(value_list) - ddof))
 
 
 def stddev(value_list: List[float]) -> float:
     return sqrt(variance(value_list))
 
+
+def median(value_list: List[float]) -> float:
+    sorted_list = sorted(value_list)
+    len_list = len(sorted_list)
+    index_middle = int(len_list/2)
+
+    if len_list % 2 == 0:
+        return float((sorted_list[index_middle-1]+sorted_list[index_middle])/2)
+    else:
+        return float(sorted_list[index_middle])
 
