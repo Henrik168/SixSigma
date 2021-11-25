da239cd44a1058be8f034cfe50d0727e7b25b55f
Henrik Schletter <62748382+Henrik168@users.noreply.github.com>
2021-11-25 18:12:17 +0100

    Add files via upload

--- test_six_sigma.py
+++ test_six_sigma.py
@@ -1,19 +1,20 @@
-from six_sigma.calc import mean
-from random import random, gauss, randrange
-from six_sigma.calc import mean, stddev
+from random import gauss, randrange, seed
 from six_sigma.plot import Plot
 
+seed(42)
 test_data = [randrange(50, 80) for _ in range(1000)]
-test_data_gaus = [min(gauss(mu=97, sigma=2), 100) for _ in range(1000)]
-test_data_gaus2 = [min(gauss(mu=90, sigma=10), 100) for _ in range(1000)]
+test_data_gauss = [min(gauss(mu=97, sigma=2), 100) for _ in range(1000)]
+test_data_gauss2 = [min(gauss(mu=90, sigma=10), 100) for _ in range(1000)]
 
 plt = Plot(y_label="Y-Label",
            x_data_label="X-Data",
            x_hist_label="X-Hist",
            y_min=0,
-           y_max=100)
-plt.add_plot(test_data_gaus, "GausData1", 100, 80)
-plt.add_plot(test_data_gaus2, "GausData2", 100, 80)
+           y_max=100,
+           bin_width=2)
+
+plt.add_plot(test_data_gauss, "GaussData1", 100, 80)
+plt.add_plot(test_data_gauss2, "GaussData2", 100, 80)
 plt.add_plot(test_data, "RandData", 75, 55)
-#plt.show_plots()
+plt.show_plots()
 plt.save_plots()
